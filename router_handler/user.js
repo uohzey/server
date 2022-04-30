77a666a058c84c256717d76f67dd8f028d2a9852
uohzey <420709076@qq.com>
2022-04-30 22:04:32 +0800

    register

--- router_handler/user.js
+++ router_handler/user.js
@@ -1,6 +1,7 @@
 //导入数据库操作模块
 import db from "../db/index.js";
-
+//导入加密模块
+import bcrypt from "bcryptjs";
 
 export function regUser(req, res) {
     //获取客户端提交到服务器的用户信息
@@ -9,8 +10,6 @@ export function regUser(req, res) {
     if (!userinfo.username || !userinfo.password) {
         return res.send({ status: 1, message: '用户名或密码不合法!' })
     }
-
-
     //定义SQL语句,查询用户名是否被使用
     const sqlStr = 'select * from ev_users where username = ?'
     db.query(sqlStr, userinfo.username, (err, results) => {
@@ -22,8 +21,19 @@ export function regUser(req, res) {
         if (results.length > 0) {
             return res.send({ status: 1, message: '用户名被占用,请更换其他用户名!' })
         }
-
-        //TODO:用户名可以使用
+        //TODO:用户名可以使用,加密,返回值是加密之后的密码
+        userinfo.password = bcrypt.hashSync(userinfo.password, 10)
+        //定义插入新用户的SQL语句
+        const sql = 'insert into ev_users set ?'
+        //调用db.query()执行SQLyuju
+        db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, results) => {
+            //判断sql语句是否执行成功
+            if (err) return res.send({ status: 1, message: err.message })
+            //判断影响行数是否为1
+            if (results.affectedRows !== 1) return res.send({ status: 1, message: '注册用户失败,请稍后再试!' })
+            //注册成功
+            res.send({ status: 0, message: '注册成功!' })
+        })
     })
 }
 
