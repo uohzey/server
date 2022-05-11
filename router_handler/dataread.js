d421728bac861953fb16e91dbfe2474fcf17da9a
uohzey <420709076@qq.com>
2022-05-11 11:02:39 +0800

    dateFormat

--- router_handler/dataread.js
+++ router_handler/dataread.js
@@ -1,4 +1,21 @@
 import db from "../db/index.js"
+function dateFormat(date, fmt) {
+    if (null == date || undefined == date) return '';
+    var o = {
+        "M+": date.getMonth() + 1, //⽉份
+        "d+": date.getDate(), //⽇
+        "h+": date.getHours(), //⼩时
+        "m+": date.getMinutes(), //分
+        "s+": date.getSeconds(), //秒
+        "S": date.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+Date.prototype.toJSON = function () { return dateFormat(this, 'yyyy-MM-dd') }
+
 
 export function getDataFromAcl(req, res) {
     //定义sql查询语句
@@ -9,12 +26,14 @@ export function getDataFromAcl(req, res) {
         if (err) {
             return res.cc(err)
         }
-        res.send(
-            {
+        // results.date = dateFormat(results.date)
+        res.send({
+            data: {
                 status: 0,
                 message: '读取数据成功!',
                 data: results,
             }
+        }
         )
     })
 }
